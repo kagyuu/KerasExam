71874aae76a92d9591714841d42dc5d6b95ffd66
Atsushi <atsushi.hondoh@gmai.com>
2024-03-17 00:45:46 +0900

    add tqdm

--- list_gpu.py
+++ list_gpu.py
@@ -1,4 +1,8 @@
 import tensorflow as tf;
+from tensorflow.python.platform import build_info as tf_build_info
 
 for physical_device in tf.config.list_physical_devices('GPU') :
     print(tf.config.experimental.get_device_details(physical_device))
+
+print("cudnn_version",tf_build_info.build_info['cudnn_version'])
+print("cuda_version",tf_build_info.build_info['cuda_version'])
