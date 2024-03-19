c48bcf64d833298d634375614334537eaaaef3ea
Atsushi <atsushi.hondoh@gmai.com>
2024-03-20 01:13:27 +0900

    add MNIST

--- python/mnist.py
+++ python/mnist.py
@@ -1,11 +1,63 @@
+from __future__ import print_function
 import numpy as np
 from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.optimizers import SGD
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
 
-# set numpy random seed for reproducibility
-np.random.seed(42)
+# Parameters
+NB_EPOCH = 20
+BATCH_SIZE = 128
+VERBOSE = 1
+NB_CLASSES = 10
+OPTIMIZER = SGD()
+N_HIDDEN = 128
+VALIDATION_SPLIT = 0.2
 
-# load mnist data from keras datasets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# random seed for reproducibility
+np.random.seed(1671)
 
-# reshape x_train from 60000x28x28 to 60000x784
-x_train = x_train.reshape(60000, 784)
+# Load MNIST dataset from Keras
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# X_train is 60000 rows of 28x28 values --> reshaped in 60000 x 784
+RESHAPED = 28 * 28
+X_train = X_train.reshape(60000, RESHAPED)
+X_test = X_test.reshape(10000, RESHAPED)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+# Normalize
+X_train /= 255.0
+X_test /= 255.0
+
+# Convert class vectors to binary class matrices
+Y_train = to_categorical(y_train, NB_CLASSES)
+Y_test = to_categorical(y_test, NB_CLASSES)
+
+print('X_train' , X_train.shape)
+print('X_test ' , X_test.shape)
+print('Y_train' , Y_train.shape)
+print('Y_test ' , Y_test.shape)
+
+print('X_train[0]', X_train[0])
+print('Y_train[0]', Y_train[0])
+
+model = Sequential()
+model.add(Dense(N_HIDDEN, input_shape=(RESHAPED,)))
+model.add(Activation('relu'))
+model.add(Dense(N_HIDDEN))
+model.add(Activation('relu'))
+model.add(Dense(NB_CLASSES))
+model.add(Activation('softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer=OPTIMIZER, metrics=['accuracy'])
+
+model.fit(X_train, Y_train, batch_size=BATCH_SIZE, epochs=NB_EPOCH, verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
+
+score = model.evaluate(X_test, Y_test, verbose=VERBOSE)
+print("Test score:", score[0])
+print("Test accuracy:", score[1])
